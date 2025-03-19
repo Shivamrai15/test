13fa2eac1078987799ffb7b9e627f4875acf1e30
Shivam <shivamrai6836@gmail.com>
2025-03-19 23:22:50 +0530

    base uri changed

--- src/auth-config.js
+++ src/auth-config.js
@@ -2,7 +2,7 @@ export const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.AUTH0_SECRET,
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://test-2pxt.onrender.com',
   clientID: process.env.AUTH0_CLIENT_ID,
   issuerBaseURL: 'https://dev-kuudrn3zb6ggs5dl.jp.auth0.com'
 };
